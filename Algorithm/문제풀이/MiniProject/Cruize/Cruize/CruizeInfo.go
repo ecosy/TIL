248837b3a098ae3e339f3a17b43085612bda9b1c
ecosy <kevin9434@naver.com>
2021-04-01 17:58:39 +0900

    add miniproject - car cruize controller codes in golang

--- Algorithm/문제풀이/MiniProject/Cruize/Cruize/CruizeInfo.go
+++ Algorithm/문제풀이/MiniProject/Cruize/Cruize/CruizeInfo.go
@@ -5,6 +5,8 @@ const (
 	UPS            // Up Short - 위로 짧게 버튼 누름
 	DNH            // Down Hold - 아래로 길게 버튼 누름
 	DNS            // Down Short - 아래로 짧게 버튼 누름
+
+	SPEED_BUFFER = 1000 // speed buffer
 )
 
 type ButtonType int
--- Algorithm/문제풀이/MiniProject/Cruize/main.go
+++ Algorithm/문제풀이/MiniProject/Cruize/main.go
@@ -1,37 +1,64 @@
 package main
 
 import (
+	"algorithm/MiniProject/Cruize/Cruize"
 	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"test/Cruize/Cruize"
 )
 
+const (
+	TERMINATE = "end"
+)
+
+func PrintCtrlMenu() {
+	fmt.Println("==== Enter Control Command ====")
+	fmt.Println(`
+	0 : UPH (Up Hold)
+	1 : UPS (Up Short)
+	2 : DNH (Down Hold)
+	3 : DNS (Down Short)
+	end : driving end`)
+	fmt.Print("> ")
+}
+
 func Drive() {
-	log.Println("Drive Func start...")
+	log.Println("Drive start...")
 	defer func() {
 		log.Println("Drive Func end...")
 		return
 	}()
 
-	// ioReader := bufio.NewReader(os.Stdin)
-	consoleReader := bufio.NewReaderSize(os.Stdin, 1)
+	var currentSpeed int = 0
+
+	// keyboard input reader
+	ioReader := bufio.NewReader(os.Stdin)
+
 	for {
-		fmt.Print("> ")
-		// command, _ := ioReader.ReadString('\n')
-		command, _ := consoleReader.ReadByte()
-		log.Printf("io input : %v\n", command)
+		PrintCtrlMenu() // print control menu
+
+		// get keyboard input
+		command, _ := ioReader.ReadString('\n')
+		log.Printf("drive input command : %v\n", command)
+
+		// 개행문자 \n 제거
+		command = command[:len(command)-2]
+
+		// 종료 조건
+		if command == TERMINATE {
+			log.Println("drive terminated....")
+			break
+		}
+
+		// command -> speed 변경
+		Cruize.CommandToSpeed(&command, &currentSpeed)
 	}
+
 }
 
 func main() {
-	log.Println("i am main")
-
 	Drive()
-
-	Cruize.CalculateCruiseControlSpeed(Cruize.UPH, 1)
-
 	return
 }
 
